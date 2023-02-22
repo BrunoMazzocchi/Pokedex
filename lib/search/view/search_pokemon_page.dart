14b65db6c191427e67daf42549eee1663c9551af
Bruno Mazzocchi <bemazzocchi@gmail.com>
2023-02-22 00:25:21 -0600

    Finished first veresion search screen

--- lib/search/view/search_pokemon_page.dart
+++ lib/search/view/search_pokemon_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pokedex/main.dart';
+import 'package:poke_client/poke_client.dart';
 import 'package:pokedex/search/cubit/pokemon_search_cubit.dart';
 
 class SearchPokemonPage extends StatefulWidget {
@@ -17,7 +17,6 @@ class _SearchPokemonPageState extends State<SearchPokemonPage> {
     return const AppView();
   }
 }
-
 
 class AppView extends StatefulWidget {
   const AppView({Key? key}) : super(key: key);
@@ -81,19 +80,20 @@ class _AppViewState extends State<AppView> {
               },
               onTap: _onClearTapped,
             ),
+            const SizedBox(height: 20),
             BlocBuilder<PokemonSearchCubit, PokemonSearchState>(
                 builder: (context, state) {
-                  switch (state.status) {
-                    case PokemonSearchStatus.loading:
-                      return const Center(child: CircularProgressIndicator());
-                    case PokemonSearchStatus.success:
-                      return Text(state.pokemon.name);
-                    case PokemonSearchStatus.error:
-                      return const Text('Error');
-                    default:
-                      return const Text('No pokemon found');
-                  }
-                })
+              switch (state.status) {
+                case PokemonSearchStatus.loading:
+                  return const LoadingCard();
+                case PokemonSearchStatus.success:
+                  return PokemonCard(pokemon: state.pokemon);
+                case PokemonSearchStatus.error:
+                  return const NotFoundCard();
+                default:
+                  return const NotFoundCard();
+              }
+            })
           ],
         ),
       ),
@@ -101,3 +101,140 @@ class _AppViewState extends State<AppView> {
   }
 }
 
+class PokemonCard extends StatefulWidget {
+  final Pokemon pokemon;
+
+  const PokemonCard({Key? key, required this.pokemon}) : super(key: key);
+
+  @override
+  State<PokemonCard> createState() => _PokemonCardState();
+}
+
+class _PokemonCardState extends State<PokemonCard> {
+  late double width = MediaQuery.of(context).size.width;
+  late double height = MediaQuery.of(context).size.height;
+  final String imageUrl =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 120,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Image.network("$imageUrl${widget.pokemon.id}.png"),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Pokemon name: ${widget.pokemon.name}"),
+              Text("Pokemon id: ${widget.pokemon.id}"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotFoundCard extends StatelessWidget {
+  const NotFoundCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: const Center(
+        child: Text(
+          "At this time we did not find your pokemon. Please check that the name or ID is correct :D",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoadingCard extends StatelessWidget {
+  const LoadingCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: CircularProgressIndicator(),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 100,
+                height: 20,
+                color: Colors.grey,
+              ),
+              Container(
+                width: 100,
+                height: 20,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
